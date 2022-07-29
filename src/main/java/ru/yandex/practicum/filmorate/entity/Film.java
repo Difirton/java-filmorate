546aac3080e82606b986e1820455e3bee2052161
Dmitriy Kruglov <difirton@yandex.ru>
2022-07-29 22:11:40 +0300

    feat - add validation to entity

--- src/main/java/ru/yandex/practicum/filmorate/entity/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/entity/Film.java
@@ -1,8 +1,12 @@
 package ru.yandex.practicum.filmorate.entity;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.Duration;
 import java.time.LocalDate;
 
@@ -18,12 +22,17 @@ public class Film {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Film name should not be empty")
     private String name;
 
+    @Length(max = 200, message = "Should be less than 200 characters")
+    @Column(length = 200)
     private String description;
 
+    @Temporal(TemporalType.DATE)
     @Column(name = "release_date")
     private LocalDate releaseDate;
 
+    @Positive(message = "Should be positive")
     private Duration duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/entity/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/entity/User.java
@@ -1,14 +1,13 @@
 package ru.yandex.practicum.filmorate.entity;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
@@ -19,16 +18,22 @@ import java.time.LocalDate;
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email should not be blank")
     @Column(unique = true, nullable = false)
     private String email;
 
+    @NotBlank
     @Column(unique = true, nullable = false)
+    @Pattern(regexp = "/^\\S*$/", message = "Login should not contain spaces")
     private String login;
 
-    private String name;
+    private String name = login;
 
+    @Past
     private LocalDate birthday;
 
 }
