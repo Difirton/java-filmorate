3981de9977fb37614ecc5e634a126dd60ef208cd
Pol <a.poltavski@yandex.ru>
2022-10-05 23:52:31 +0300

    feat: add selecting most popular film by genreId and/or year of release

--- src/main/java/ru/yandex/practicum/filmorate/repository/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/FilmRepository.java
@@ -1,12 +1,16 @@
 package ru.yandex.practicum.filmorate.repository;
 
+import org.springframework.expression.spel.ast.OpAnd;
 import ru.yandex.practicum.filmorate.entity.Film;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FilmRepository extends StandardCRUDRepository<Film> {
 
     List<Film> findPopularFilmsByRate(Integer count);
+
+    List<Film> findPopularFilmsByRate(Integer count, Integer genreID, Integer year);
 
     int[][] updateAll(List<Film> films);
 
