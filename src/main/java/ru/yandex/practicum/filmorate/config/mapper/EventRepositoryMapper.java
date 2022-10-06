fb2d19a445bf7c18fef64d0cbc9b9a027a9f9a6d
Artur <am29-10@mail.ru>
2022-10-06 11:29:55 +0300

    <feat> - add event

--- src/main/java/ru/yandex/practicum/filmorate/config/mapper/EventRepositoryMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/config/mapper/EventRepositoryMapper.java
@@ -14,7 +14,7 @@ public class EventRepositoryMapper implements RowMapper<Event> {
     @Override
     public Event mapRow(ResultSet rs, int rowNum) throws SQLException {
         return Event.builder()
-                .eventId(rs.getLong("id"))
+                .id(rs.getLong("id"))
                 .timestamp(rs.getLong("timestamp"))
         .userId(rs.getLong("user_id"))
         .eventType(EventTypes.valueOf(rs.getString("event_type")))
