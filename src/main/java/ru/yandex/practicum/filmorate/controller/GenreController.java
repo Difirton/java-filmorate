4112c0d2056c4e99aa62d58cabe86cfee40a1503
Difirton <difirton@yandex.ru>
2022-10-07 23:05:59 +0300

    <feat> - add SwaggerConfig <feat> - add swagger's configuration to endpoints

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -1,5 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +19,33 @@ import java.util.List;
 public class GenreController {
     private final GenreService genreService;
 
+    @Operation(summary = "Get all movie genres", tags = "genre")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found the genres",
+                    content = {
+                            @Content(mediaType = "application/json")
+                    })
+    })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<Genre> findAllGenres() {
         return genreService.getAllGenres();
     }
 
+    @Operation(summary = "Get the genre by his id, which is specified in URL", tags = "genre")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Returns the requested genre",
+                    content = {
+                            @Content(mediaType = "application/json")
+                    })
+    })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
-    public Genre findGenre(@PathVariable("id") Long id) {
+    public Genre findGenre(@PathVariable("id") @Parameter(description = "The genre ID") Long id) {
         return genreService.getGenreById(id);
     }
 }
